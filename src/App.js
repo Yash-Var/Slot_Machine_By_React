ad512bb1688b6b755414b36fa4a890bfcee42a90
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-06-27 23:17:14 +0530

    completed the slot_Machine_game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import "./App.css";
-const App=()=>{
-return(
-  <>
-    <h1 className='heading_style'>🎰 Welcome to <span style={{fontWeight:"bold", backgroundColor:"white"}}>Slot Machine</span></h1>
-  </>
-);
+
+const SlotM = () => {
+  let x = "😄";
+  let y = "😄";
+  let z = "👍";
+  if (x === y && x === z) {
+    return (
+      <>
+        <div className='slot_inner'>
+          <h1>{x}{y}{z}</h1>
+          <h1>This is Matching. </h1>
+          <hr />
+        </div>
+      </>
+    );
+  }else{
+    return (
+      <>
+        <div className='slot_inner'>
+          <h1>{x}{y}{z}</h1>
+          <h1>This is Matching. </h1>
+          <hr />
+        </div>
+      </>
+    );
+  }
+};
+
+const App = () => {
+  return (
+    <>
+      <h1 className='heading_style'>🎰 Welcome to <span style={{ fontWeight: "bold", backgroundColor: "white" }}>Slot Machine</span></h1>
+      <div className='slot_M'>
+      <SlotM />
+      <SlotM />
+      <SlotM />
+      </div>
+    </>
+  );
 };
 export default App;
