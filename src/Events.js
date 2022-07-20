825f5325347e24fa24c025c47fe3c00b68017e5c
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-07-20 15:54:21 +0530

    events in React js

---
+++ src/Events.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+const Events=()=>{
+    return (
+        <>
+   <div>
+    <button>Click Me </button>
+   </div>
+        </>
+    );
+}
+export default Events;
