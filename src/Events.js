8d121ea6d0a928e254cd3ea5f793b0fc3468f47f
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-07-20 16:04:09 +0530

    events css added

--- src/Events.js
+++ src/Events.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
+// import './Events.css';
 const Events=()=>{
     return (
         <>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import  ReactDOM  from "react-dom";
 import App from "./App.js";
 import Hooks from "./Hooks.js";
 import Fetchtime from "./Fetchtime.js";
-import "./hooks.css";
+// import "./hooks.css";
 import CurrentTime from "./CurrentTime.js";
-import "./App.css";
+// import "./App.css";
+import Events from './Events';
+import './Events.css';
 ReactDOM.render(
   <>
+  <Events />
    {/* <App />
    <CurrentTime />
    <Hooks />
