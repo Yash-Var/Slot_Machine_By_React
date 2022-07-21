a9085b3c1574f5a25234abe42cf84d96130591fa
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-07-21 22:59:47 +0530

    updation

--- src/Events.js
+++ src/Events.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
 // import './Events.css';
+
 const Events=()=>{
-    return (
-        <>
-   <div>
-    <button>Click Me </button>
-   </div>
-        </>
-    );
+    const bgChanges=()=>{
+        console.log("clicked");
+    }
+//     return (
+//         <>
+//    <div style={{backgroundColor:"red"}}>
+//     <button onClick={bgChanges}> Click Me </button>
+
+//    </div>
+//         </>
+//     );
 }
-export default Events;
+// export default Events; 
