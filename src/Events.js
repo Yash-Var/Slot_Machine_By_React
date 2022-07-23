279a3f5e65594864af7eae2917d8ff63a784b553
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-07-23 17:00:05 +0530

    background color changes dynamically

--- src/Events.js
+++ src/Events.js
@@ -1,18 +1,31 @@
 // import React from 'react';
-// import  ReactDOM  from 'react-dom';
-// import './Events.css';
+import  ReactDOM  from 'react-dom';
+import React, { useState } from "react";
+import './Events.css';
 
-// const Events=()=>{
-//     const bgChanges=()=>{
-//         console.log("clicked");
-//     }
-//     return (
-//         <>
-//    <div style={{backgroundColor:"red"}}>
-//     <button onClick={bgChanges}> Click Me </button>
+const Events=()=>{
+    const purple='#8e44ad';
+   const [bg, setbg]=useState(purple);
+    const bgChanges=()=>{
+        // console.log("clicked");
+        let newbg;
+        if(bg==='#8e44ad'){
 
-//    </div>
-//         </>
-//     );
-// }
-// export default Events; 
+             newbg='#34495e';
+            setbg(newbg);   
+        }else{
+             newbg='#8e44ad';
+            setbg(newbg);   
+
+        }
+    }
+    return (
+        <>
+   <div style={{backgroundColor:bg}}>
+    <button onClick={bgChanges}> Click Me </button>
+
+   </div>
+        </>
+    );
+ }
+export default Events; 
